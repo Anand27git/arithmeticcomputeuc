cab62cd549bf8cb74878cc1e1acaceeb0ffaa81e
Anand27git <anandkumar256k@gmail.com>
2021-05-08 19:41:50 +0530

    commit success

--- arithemtic.sh
+++ arithemtic.sh
@@ -2,3 +2,8 @@
 
 echo "welcome to the arithimetic computation problems"
 
+read -p "enter the value of a: " a
+read -p "enter the value of b: " b
+read -p "enter the value of c: " c
+echo $a $b $c
+
